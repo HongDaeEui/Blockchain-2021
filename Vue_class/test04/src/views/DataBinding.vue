5e7a0ebf3f15790b02fedb60387e92b1716f5267
PastelWhale <43264739+HongDaeEui@users.noreply.github.com>
2021-07-02 17:36:47 +0900

    class

---
+++ Vue_class/test04/src/views/DataBinding.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div>
+            <h1>Hello {{title1}}</h1>
+        </div>
+        <div>
+            <h1>Hello {{title2}}</h1>
+            <!-- 중괄호 두 개 써야함 -->
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: "",
+    components: {},
+    data() {
+        return {
+            title1: "World",
+            title2:"Day",
+            //전역변수 선언
+        };
+    },
+    setup() {},
+    created() {},
+    mounted() {},
+    unmounted() {},
+    methods: {}
+}
+</script>
